dc27fe8733759fb11616d20b35b6ecf58bb76c1d
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2023-12-20 16:52:21 +0500

    src

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Herosection from './components/Herosection';
 import Section2 from './components/Section2';
+import Future from './components/Future';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <Herosection />
       <Section2 />
+      <Future />
+      <Footer />
     </div>
   );
 }
